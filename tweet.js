279bc2f31fcec235f70118d71bee1512b9389f13
socraticDevBlog <maximebonin@outlook.com>
2020-11-19 22:16:53 -0500

    ok ok ok

--- tweet.js
+++ tweet.js
@@ -23,7 +23,6 @@ if (today.getMonth()==11 && today.getDate()>25)
 var one_day=1000*60*60*24;
 var daysToXmas = Math.ceil((xmas.getTime()-today.getTime())/(one_day));
 
-
 const message = `👋 Hey there dailyfolks!.
 This is an automated message 🤖 to remind you that there's ${daysToXmas} days left to Christmas ♥️`;
 
