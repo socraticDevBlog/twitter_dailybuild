3e408b3d0117b84f9ee474bf807904d0e116af8a
socraticDevBlog <maximebonin@outlook.com>
2020-11-21 17:42:43 -0500

    adding tweetposter module

--- regular-tweet.js
+++ regular-tweet.js
@@ -1,36 +1,9 @@
-const Twit = require("twit");
-
-// keys are already taken care of. github action will push them in here for us
-//
-const {
-  CONSUMER_KEY,
-  CONSUMER_SECRET,
-  ACCESS_TOKEN,
-  ACCESS_TOKEN_SECRET,
-} = process.env;
-
-const T = new Twit({
-  consumer_key: CONSUMER_KEY,
-  consumer_secret: CONSUMER_SECRET,
-  access_token: ACCESS_TOKEN,
-  access_token_secret: ACCESS_TOKEN_SECRET,
-});
-
 // populate the message you want to tweet, they submit a pull-request
 // ** if there is already a message in there: replace it with yours
 //
-var message = "any members of #dailybuild community can tweet from this account via github :)"; //<------- *** enter your tweet ***
+const message = "not tweeting makes danny a dull boi"; //<------- *** enter your tweet ***
+//
+// that's it ! dont worry about the code down under.
 
-if (message) {
-  T.post("statuses/update", { status: message }, function (
-    err,
-    data,
-    response
-  ) {
-    if (err) {
-      console.log("caught error", err.stack);
-    } else {
-      console.log(data);
-    }
-  });
-}
+let tweeter = require(tweet - module);
+let posted = tweeter.TweetPoster(message);
---
+++ tweet-module.js
@@ -0,0 +1,38 @@
+const Twit = require("twit");
+
+const {
+  CONSUMER_KEY,
+  CONSUMER_SECRET,
+  ACCESS_TOKEN,
+  ACCESS_TOKEN_SECRET,
+} = process.env;
+
+const T = new Twit({
+  consumer_key: CONSUMER_KEY,
+  consumer_secret: CONSUMER_SECRET,
+  access_token: ACCESS_TOKEN,
+  access_token_secret: ACCESS_TOKEN_SECRET,
+});
+
+class TweetPoster {
+    constructor(msg) {
+        this.msg = msg;
+        if (message) {
+            T.post(
+                "statuses/update",
+                { status: message },
+                function (err, data, response) {
+                    if (err) {
+                        console.log("caught error", err.stack);
+                    } else {
+                        console.log(data);
+                    }
+                }
+            );
+        }
+    }
+}
+
+module.exports = {
+  TweetPoster: TweetPoster,
+};
