9afcc0b8d67a7adaa6d73f750fdb32f663c79137
socraticDevBlog <41236715+socraticDevBlog@users.noreply.github.com>
2020-11-21 18:03:41 -0500

    Update regular-tweet.js

--- regular-tweet.js
+++ regular-tweet.js
@@ -1,7 +1,7 @@
-// populate the message you want to tweet, they submit a pull-request
+// populate the message you want to tweet, then submit a pull-request
 // ** if there is already a message in there: replace it with yours
 //
-const message = "all @dailybuild3's tweet comes from a push to main branch https://github.com/socraticDevBlog/twitter_dailybuild 💡"; //<------- *** enter your tweet ***
+const message = "... except for kenster 🧔"; //<------- *** enter your tweet ***
 //
 // that's it ! dont worry about the code down under.
 
