5430ddd2efd1e1710757b278203038a304755694
socraticDevBlog <maximebonin@outlook.com>
2020-11-26 19:26:00 -0500

    cleaned up  post  if conversion base64 is success

--- flat-earth-tweet.js
+++ flat-earth-tweet.js
@@ -3,16 +3,15 @@ const data = require("./flat-earth-data");
 
 const prefix = "https://globie-info.neet.cloud/";
 const randomId = utilFct.randomizer(1, Object.keys(data).length);
-let imgUrl = prefix + (data[randomId]).name;
+let imgUrl = prefix + data[randomId].name;
 
-var request = require('request').defaults({ encoding: null });
+var request = require("request").defaults({ encoding: null });
 
 request.get(imgUrl, function (error, response, body) {
-    if (!error && response.statusCode == 200) {
-        // payload = "data:" + response.headers["content-type"] + ";base64," + Buffer.from(body).toString('base64');
-        payload = Buffer.from(body).toString('base64');
-    }
+  if (!error && response.statusCode == 200) {
+    // payload = "data:" + response.headers["content-type"] + ";base64," + Buffer.from(body).toString('base64');
+    payload = Buffer.from(body).toString("base64");
+    let tweeter = require("./tweet-module");
+    let posted = new tweeter.TweetMediaPoster(payload);
+  }
 });
-
-let tweeter = require("./tweet-module");
-let posted = new tweeter.TweetMediaPoster(payload);
