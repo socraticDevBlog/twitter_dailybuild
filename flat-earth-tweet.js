363ec68caa0338e3c4c7907303f42b4d6ca6b89f
socraticDevBlog <maximebonin@outlook.com>
2020-11-26 20:59:07 -0500

    adding messages to flatearth tweets

--- flat-earth-tweet.js
+++ flat-earth-tweet.js
@@ -1,12 +1,21 @@
 const utilFct = require("./utils");
 const data = require("./flat-earth-data");
-const tweetMsg = "omg! another one for the globies🌐";
+
+const messages = [
+  "omg! another one for the globies 🌐",
+  "flat earthers: get real ... quick!!💨",
+  "conspiracy is just dumb 🤦",
+  "🧙🏾 do u also believe in cooties, flat-earthers? 😛",
+  "🧭 get real. it's about time, globies ⌛"
+];
 
 const prefix = "https://globie-info.neet.cloud/";
 const randomId = utilFct.randomizer(1, Object.keys(data).length);
 let imgUrl = prefix + data[randomId].name;
 
 var request = require("request").defaults({ encoding: null });
+
+const tweetMsg = messages[utilFct.randomizer(0, messages.length - 1)];
 
 request.get(imgUrl, function (error, response, body) {
   if (!error && response.statusCode == 200) {
