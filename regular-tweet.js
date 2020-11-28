d944c2d317544e2bc7e32b321d50d7a30365937e
socraticDevBlog <maximebonin@outlook.com>
2020-11-28 18:57:45 -0500

    congrats luxemboye

--- regular-tweet.js
+++ regular-tweet.js
@@ -1,7 +1,7 @@
 // populate the message you want to tweet, then submit a pull-request
 // ** if there is already a message in there: replace it with yours
 //
-const message = ""; //<------- *** enter your tweet ***
+const message = "✨✨ major head pats to member 'luxemboye' who crafted a based dailyscript✨✨"; //<------- *** enter your tweet ***
 //
 // that's it ! dont worry about the code down under.
 
