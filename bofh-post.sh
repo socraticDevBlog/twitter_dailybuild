e982834b38f4eb7d515042f0f5ec0d2bd4bb059d
luxemboye <75177432+luxemboye@users.noreply.github.com>
2020-11-28 23:05:23 +0000

    Adding the BOFH post script
    Chooses a line from the bofh-excuses file depending on the date and executes post-to-twitter with the message.
    
    post-to-twitter allows to use the whole nodejs posting libraries stuff, without having to write JS code.
    Example: in the case of `node post-to-twitter.js "Message is here"`, "Message is here" will be posted

---
+++ bofh-post.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+START_DATE=$(date --date="November 29, 2020" '+%s')
+TODAY=$(date '+%s')
+DAYS=$(( ($TODAY - $START_DATE) / 86400 ))
+
+QUOTELINES=$(cat $1 | wc -l)
+FILELINE=$(( ($DAYS % $QUOTELINES) + 1 ))
+
+QUOTE=$(sed "${FILELINE}q;d" $1)
+MESSAGE="Computer brokie: $QUOTE"
+node post-to-twitter.js "$MESSAGE"
