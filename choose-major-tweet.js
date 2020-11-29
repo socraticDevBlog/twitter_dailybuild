27f98261b1fc8f0f2b6009e3ef6f4afebcc2e4d8
socraticDevBlog <maximebonin@outlook.com>
2020-11-28 20:14:25 -0500

    new job finding a major in college

---
+++ choose-major-tweet.js
@@ -0,0 +1,59 @@
+const csv = require("csvtojson");
+const utils = require("./utils");
+let tweeter = require("./tweet-module");
+
+class Major {
+  set Major(Major) {
+    this._Major = Major;
+  }
+  get Major() {
+    return this._Major;
+  }
+  set Major_category(Major_category) {
+    this._Major_category = Major_category;
+  }
+  get Major_category() {
+    return this._Major_category;
+  }
+  set Unemployment_rate(Unemployment_rate) {
+    this._Unemployement_rate = Unemployment_rate;
+  }
+  get Unemployment_rate() {
+    return this._Unemployement_rate;
+  }
+  set Median(Median) {
+    this._Median = Median;
+  }
+  get Median() {
+    return this._Median;
+  }
+
+  constructor() {}
+}
+
+
+function msg(maj) {
+    return `picking a major in college is hard!😓 did you know, in 2014, recent graduates in ${
+        maj.Major
+    } earned an average of ${maj.Median}$ unless they where part of the ${(
+        maj.Unemployment_rate * 100
+        ).toFixed(2)} % who remained unemployed. (source: https://53eig.ht/33qBbVy)`;
+    }
+    
+let majors = [];
+
+const converter = csv()
+  .fromFile("./recent-grads-data.csv")
+  .then((json) => {
+    let m;
+    json.forEach((row) => {
+      m = new Major();
+      Object.assign(m, row);
+      majors.push(m);
+    });
+  })
+  .then(() => {
+    let posted = new tweeter.TweetPoster(
+      msg(majors[utils.randomizer(0, majors.length)])
+    );
+  });
