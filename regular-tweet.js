a82b6adff9f282c0dda1a7aa9deb52314e93a212
socraticDevBlog <maximebonin@outlook.com>
2020-11-29 09:18:19 -0500

    adding a constant being more clean boi

--- regular-tweet.js
+++ regular-tweet.js
@@ -1,5 +1,6 @@
 let tweeter = require("./tweet-module");
 
+const NO_ARGS_PASSED_COUNT = 2;
 const COMMAND_LINE_ARGS_1_IDX = 2;
 
 // populate the message you want to tweet, then submit a pull-request
@@ -10,7 +11,7 @@ var message = ""; //<------- *** enter your tweet ***
 // that's it ! dont worry about the code down under.
 
 // if msg passed as command-line argument, overwrite original message
-if (process.argv.length > 2) {
+if (process.argv.length > NO_ARGS_PASSED_COUNT) {
   message = process.argv[COMMAND_LINE_ARGS_1_IDX];
 }
 
