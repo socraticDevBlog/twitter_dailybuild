1c287e8f824bd8ec6bf842e886be0a492e438ae5
socraticDev <maximebonin@outlook.com>
2020-12-05 11:10:23 -0500

    correction to cutoff

--- christmas-tweet.js
+++ christmas-tweet.js
@@ -1,21 +1,23 @@
+let tweeter = require("./tweet-module");
+
 // stolen from https://www.w3resource.com/javascript-exercises/javascript-basic-exercise-9.php (2020-11-19)
+let xmasAdvent = new Date(today.getFullYear(), 11, 25);
+let xmas = new Date(2020, 12, 25);
 let today = new Date();
-let xmas = new Date(today.getFullYear(), 11, 25);
 
 // we dont want to think about xmas anymore past december 25th
-if (today > xmas ) {
+if (today > xmas) {
   return;
 }
 
 if (today.getMonth() == 11 && today.getDate() > 25) {
-  xmas.setFullYear(xmas.getFullYear() + 1);
+  xmasAdvent.setFullYear(xmasAdvent.getFullYear() + 1);
 }
 
 let one_day = 1000 * 60 * 60 * 24;
-let daysToXmas = Math.ceil((xmas.getTime() - today.getTime()) / one_day);
+let daysToXmas = Math.ceil((xmasAdvent.getTime() - today.getTime()) / one_day);
 
 const message = `👋 Hey there dailyfolks!.
 This is an automated message 🤖 to remind you that there are ${daysToXmas} days left before Christmas 🌟🧑‍🎄`;
 
-let tweeter = require("./tweet-module");
 let posted = new tweeter.TweetPoster(message);
