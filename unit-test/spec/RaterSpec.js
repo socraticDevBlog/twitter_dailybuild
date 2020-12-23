eae74feac4e5e78e5cbc7e24f930aa31bfbff662
socraticDevBlog <maximebonin@outlook.com>
2020-12-23 12:56:24 -0500

    added one more expect test case to avoid infinity

--- unit-test/spec/RaterSpec.js
+++ unit-test/spec/RaterSpec.js
@@ -14,14 +14,13 @@ describe("Rater", function () {
 
     let result = rater.rateWithPoints(upvotes, downvotes);
 
+    expect(result).not.toEqual(infinity);
     expect(result).toEqual(0);
   });
 
   it ("should give a better score to a better meme", function () {
     let basedMemeUpvotes = 123;
     let basedMemeDownvotes = 1;
-
-    console.log('ran');
 
     let unbasedMemeUpvotes = 1;
     let unbasedMemeDownvotes = 123;
