29c9f7eb53331249aeee5ef455cb27b0c05f156e
socraticDev <maximebonin@outlook.com>
2021-02-24 17:18:16 -0500

    fixed news tweet adding logging

--- daily-news.js
+++ daily-news.js
@@ -1,34 +1,38 @@
-const axios = require('axios');
+const axios = require("axios");
 const util = require("./utils");
 
-const {
-  NEWS_API_KEY
-} = process.env;
+const { NEWS_API_KEY } = process.env;
 
 const URL = `https://newsapi.org/v2/everything?excludeDomains=mashable.com,techcrunch.com,slashdot.org,engadget.com&sortBy=relevancy&q=programming%20and%20language&apiKey=${NEWS_API_KEY}`;
 
-axios.get(URL)
-  .then(response => {
+axios
+  .get(URL)
+  .then((response) => {
     let data = response.data;
     const randomId = util.randomizer(0, Object.keys(data.articles).length);
-    let article = data.articles[randomId];
-    let title = article.title;
-    let urlToImage = article['urlToImage'];
 
-    postTweet(title, urlToImage);
+    let article = data.articles[randomId];
+    let msg = article.title;
+    msg += " " + article.url;
+    let urlToImage = article["urlToImage"];
+
+    console.log(`msg: ${msg}`);
+    console.log(`image url: ${urlToImage}`);
+
+    postTweet(msg, urlToImage);
   })
-  .catch(error => {
+  .catch((error) => {
     console.log(error);
   });
 
-  function postTweet(msg, imgUrl) {
-    let request = require("request").defaults({ encoding: null });
-  
-    request.get(imgUrl, function (error, response, body) {
-      if (!error && response.statusCode == 200) {
-        img = Buffer.from(body).toString("base64");
-        let tweeter = require("./tweet-module");
-        let _ = new tweeter.TweetMediaPoster(msg, img);
-      }
-    });
-  }
+function postTweet(msg, imgUrl) {
+  let request = require("request").defaults({ encoding: null });
+
+  request.get(imgUrl, function (error, response, body) {
+    if (!error && response.statusCode == 200) {
+      img = Buffer.from(body).toString("base64");
+      let tweeter = require("./tweet-module");
+      let _ = new tweeter.TweetMediaPoster(msg, img);
+    }
+  });
+}
