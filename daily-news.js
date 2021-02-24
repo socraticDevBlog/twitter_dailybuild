a6995add725fae5a4de50933190f37da2e767ff1
socraticDev <maximebonin@outlook.com>
2021-02-24 12:53:16 -0500

    new tweet job daily-news

---
+++ daily-news.js
@@ -0,0 +1,33 @@
+import { get } from 'axios';
+import { randomizer } from "./utils";
+
+const {
+    NEWS_API_KEY
+  } = process.env;
+  
+const URL = `https://newsapi.org/v2/everything?excludeDomains=mashable.com,techcrunch.com,slashdot.org,engadget.com&sortBy=relevancy&q=programming%20and%20language&apiKey=${NEWS_API_KEY}`;
+
+function postTweet(msg, img) {
+    var request = require("request").defaults({ encoding: null });
+
+    request.get(imgUrl, function (error, response, body) {
+      if (!error && response.statusCode == 200) {
+        img = Buffer.from(body).toString("base64");
+        let tweeter = require("./tweet-module");
+        let _ = new tweeter.TweetMediaPoster(msg, img);
+      }
+    });
+}
+
+get(URL)
+  .then(response => {
+    let data = response.data;
+    const randomId = randomizer(0, Object.keys(data.articles).length);
+    let title = data.articles[randomId].title;
+    let urlToImage = data.articles[randomId].urlToImage;
+
+    postTweet(title, urlToImage);
+  })
+  .catch(error => {
+    console.log(error);
+  });
