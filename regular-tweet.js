af7f0a9f2fc1e646b95734dfd8a6fb94d1e28555
socraticDevBlog <41236715+socraticDevBlog@users.noreply.github.com>
2021-03-30 16:14:06 -0400

    Update regular-tweet.js

--- regular-tweet.js
+++ regular-tweet.js
@@ -6,7 +6,7 @@ const COMMAND_LINE_ARGS_1_IDX = 2;
 // populate the message you want to tweet, then submit a pull-request
 // ** if there is already a message in there: replace it with yours
 //
-var message = ""; //<------- *** enter your tweet ***
+var message = "dailybuild is noticing our bot is tweeting garbage recently. not our fault but the news api. Will get fixed very soon! bear with us🐻"; //<------- *** enter your tweet ***
 //
 // that's it ! dont worry about the code down under.
 
