ae477e9abd6d5ab8be8d61d7c4b8bf20db3845e2
socraticDevBlog <41236715+socraticDevBlog@users.noreply.github.com>
2021-04-04 14:22:30 -0400

    tweet about beat time

--- regular-tweet.js
+++ regular-tweet.js
@@ -6,7 +6,7 @@ const COMMAND_LINE_ARGS_1_IDX = 2;
 // populate the message you want to tweet, then submit a pull-request
 // ** if there is already a message in there: replace it with yours
 //
-var message = "dailybuild is noticing our bot is tweeting garbage recently. not our fault but the news api. Will get fixed very soon! bear with us🐻"; //<------- *** enter your tweet ***
+var message = "@PJVogt our small internet community uses swacth beattime (ref to tldr episode), no?? https://rizon.dailybuild.org/#first"; //<------- *** enter your tweet ***
 //
 // that's it ! dont worry about the code down under.
 
