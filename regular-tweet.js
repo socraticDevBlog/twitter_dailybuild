8fb801a75523a97fa4c2003aebe11a70927460e9
socraticDev <maximebonin@outlook.com>
2021-04-24 13:44:16 -0400

    reset tweet message

--- regular-tweet.js
+++ regular-tweet.js
@@ -6,7 +6,7 @@ const COMMAND_LINE_ARGS_1_IDX = 2;
 // populate the message you want to tweet, then submit a pull-request
 // ** if there is already a message in there: replace it with yours
 //
-var message = "Next movie is Weekend at Bernie's on Sunday 18th at 04:30PM. Join us at #dailybuild on rizon.net"; //<------- *** enter your tweet ***
+var message = ""; //<------- *** enter your tweet ***
 //
 // that's it ! dont worry about the code down under.
 
