059c451396a035000c093882f202ea6b467e8e7d
socraticDevBlog <41236715+socraticDevBlog@users.noreply.github.com>
2021-04-30 11:48:47 -0400

    Update regular-tweet.js

--- regular-tweet.js
+++ regular-tweet.js
@@ -6,7 +6,7 @@ const COMMAND_LINE_ARGS_1_IDX = 2;
 // populate the message you want to tweet, then submit a pull-request
 // ** if there is already a message in there: replace it with yours
 //
-var message = "Next Movie we riff: Donnie Darko @ Fri April 30th 10:30EST / 7:30PST https://www.imdb.com/title/tt0246578/"; //<------- *** enter your tweet ***
+var message = "TONITE!!! we are watching: Donnie Darko @ Fri April 30th 10:30EST / 7:30PST https://www.imdb.com/title/tt0246578/"; //<------- *** enter your tweet ***
 //
 // that's it ! dont worry about the code down under.
 
