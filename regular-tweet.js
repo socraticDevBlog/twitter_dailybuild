3367397ed4a9c86cde22697e037c6742862c1e45
socraticDev <maximebonin@outlook.com>
2021-07-18 11:53:42 -0400

    annonce stop meme tweets

--- regular-tweet.js
+++ regular-tweet.js
@@ -6,8 +6,7 @@ const COMMAND_LINE_ARGS_1_IDX = 2;
 // populate the message you want to tweet, then submit a pull-request
 // ** if there is already a message in there: replace it with yours
 //
-var message = "This Friday night!!! we are watching: Gattaca @ Fri May 14th 10:30EST / 7:30PST https://www.imdb.com/title/tt0119177/"; //<------- *** enter your tweet ***
-//
+var message = "this account stopped tweeting memes. We'll transition to another kind of daily tweents. Hold on!"
 // that's it ! dont worry about the code down under.
 
 // if msg passed as command-line argument, overwrite original message
