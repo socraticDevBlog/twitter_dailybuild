7bc904ef915386c5e5d4115bd8b7ce3bad98b7ea
socraticDevBlog <41236715+socraticDevBlog@users.noreply.github.com>
2021-08-14 11:01:34 -0400

    annoncing gemini site

--- regular-tweet.js
+++ regular-tweet.js
@@ -6,7 +6,7 @@ const COMMAND_LINE_ARGS_1_IDX = 2;
 // populate the message you want to tweet, then submit a pull-request
 // ** if there is already a message in there: replace it with yours
 //
-var message = "OMG!  I love to tweet and all Earth can appreciate my wit😹😹"
+var message = "♊︎♊︎ our site now available to your gemini client gemini://dailybuild.org ♊︎♊︎ for more info about gemini protocol visit https://gemini.circumlunar.space/"
 // that's it ! dont worry about the code down under.
 
 // if msg passed as command-line argument, overwrite original message
