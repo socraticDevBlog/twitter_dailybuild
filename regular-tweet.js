cefca18eb25ab486cc764256075b76968c1b1b85
socraticDevBlog <41236715+socraticDevBlog@users.noreply.github.com>
2021-10-15 22:32:54 -0400

    d3d1ety algohader project

--- regular-tweet.js
+++ regular-tweet.js
@@ -6,7 +6,7 @@ const COMMAND_LINE_ARGS_1_IDX = 2;
 // populate the message you want to tweet, then submit a pull-request
 // ** if there is already a message in there: replace it with yours
 //
-var message = "♊︎♊︎ our site now available to your gemini client gemini://dailybuild.org ♊︎♊︎ for more info about gemini protocol visit https://gemini.circumlunar.space/"
+var message = "check out this project by our friend d3d1rty https://github.com/d3d1rty/algoheader"
 // that's it ! dont worry about the code down under.
 
 // if msg passed as command-line argument, overwrite original message
