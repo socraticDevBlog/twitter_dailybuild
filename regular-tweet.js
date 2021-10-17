9f5f74286e5774682ba6059a0c076c37103be40e
socraticDevBlog <41236715+socraticDevBlog@users.noreply.github.com>
2021-10-17 16:01:38 -0400

    quebec-art tweet

--- regular-tweet.js
+++ regular-tweet.js
@@ -6,7 +6,7 @@ const COMMAND_LINE_ARGS_1_IDX = 2;
 // populate the message you want to tweet, then submit a pull-request
 // ** if there is already a message in there: replace it with yours
 //
-var message = "check out this project by our friend d3d1rty https://github.com/d3d1rty/algoheader"
+var message = "Quebec City open air art gallery https://quebec-art.freshdatasource.com/ this is pretty based art, no?? #Canada #quebec #art #graffiti"
 // that's it ! dont worry about the code down under.
 
 // if msg passed as command-line argument, overwrite original message
