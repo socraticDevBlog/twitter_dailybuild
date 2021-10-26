106f8cd2e05ee4057c51883922902219db783287
socraticDevBlog <41236715+socraticDevBlog@users.noreply.github.com>
2021-10-26 18:54:38 -0400

    daily build gifts

--- regular-tweet.js
+++ regular-tweet.js
@@ -6,7 +6,7 @@ const COMMAND_LINE_ARGS_1_IDX = 2;
 // populate the message you want to tweet, then submit a pull-request
 // ** if there is already a message in there: replace it with yours
 //
-var message = "Quebec City open air art gallery https://quebec-art.freshdatasource.com/ this is pretty based art, no?? #Canada #quebec #art #graffiti"
+var message = "the company tasked to make 25 dailybuild gifts just sent Dailybuild a email. they finished making the items and will be shipping them to our headquarters :)"
 // that's it ! dont worry about the code down under.
 
 // if msg passed as command-line argument, overwrite original message
