355e2123fe106fc504e9bfee74798058e883e839
socraticDevBlog <41236715+socraticDevBlog@users.noreply.github.com>
2021-12-29 11:16:47 -0500

    Update regular-tweet.js

--- regular-tweet.js
+++ regular-tweet.js
@@ -6,7 +6,7 @@ const COMMAND_LINE_ARGS_1_IDX = 2;
 // populate the message you want to tweet, then submit a pull-request
 // ** if there is already a message in there: replace it with yours
 //
-var message = "@eggbertx all ur frens at #dailybuild are wishing you a great December First :))))"
+var message = "we like cute bears here!! https://0x5f3759df.xyz/files/uploads/download_20211229_104355.jpg"
 // that's it ! dont worry about the code down under.
 
 // if msg passed as command-line argument, overwrite original message
