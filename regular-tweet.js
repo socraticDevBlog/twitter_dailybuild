c2c35af117c66ab8cda600c6a38e70f6e9198c2f
socraticDevBlog <41236715+socraticDevBlog@users.noreply.github.com>
2022-01-26 13:44:13 -0500

    gemini tweet

--- regular-tweet.js
+++ regular-tweet.js
@@ -6,7 +6,7 @@ const COMMAND_LINE_ARGS_1_IDX = 2;
 // populate the message you want to tweet, then submit a pull-request
 // ** if there is already a message in there: replace it with yours
 //
-var message = "we like cute bears here!! https://0x5f3759df.xyz/files/uploads/download_20211229_104355.jpg"
+var message = "dailybuild embraced gemini protocol @soapdog : gemini://dailybuild.org #gemini we ❤️ u!!!"
 // that's it ! dont worry about the code down under.
 
 // if msg passed as command-line argument, overwrite original message
