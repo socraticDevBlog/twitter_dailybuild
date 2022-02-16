776d5e816cc6f1cde82b938fbe9c4764a6a18481
socraticDevBlog <41236715+socraticDevBlog@users.noreply.github.com>
2022-02-16 14:57:20 -0500

    Update regular-tweet.js

--- regular-tweet.js
+++ regular-tweet.js
@@ -6,7 +6,7 @@ const COMMAND_LINE_ARGS_1_IDX = 2;
 // populate the message you want to tweet, then submit a pull-request
 // ** if there is already a message in there: replace it with yours
 //
-var message = "dailybuild embraced gemini protocol @soapdog : gemini://dailybuild.org #gemini we ❤️ u!!!"
+var message = "🥳🥳today dailybuild is 2 year old.  Happy lvl up day!!!🎂🎂"
 // that's it ! dont worry about the code down under.
 
 // if msg passed as command-line argument, overwrite original message
