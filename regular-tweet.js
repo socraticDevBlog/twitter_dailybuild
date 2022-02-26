f1bf192c03e402cb41cc2a65549127ddd1042d65
socraticDevBlog <41236715+socraticDevBlog@users.noreply.github.com>
2022-02-25 21:23:15 -0500

    Update regular-tweet.js

--- regular-tweet.js
+++ regular-tweet.js
@@ -6,7 +6,7 @@ const COMMAND_LINE_ARGS_1_IDX = 2;
 // populate the message you want to tweet, then submit a pull-request
 // ** if there is already a message in there: replace it with yours
 //
-var message = "@tacixat you can checkout, but you can never leave HOTEL CALIFORNIA"
+var message = "@tacixat another cool game for your garage https://putin.icu/, no??"
 // that's it ! dont worry about the code down under.
 
 // if msg passed as command-line argument, overwrite original message
