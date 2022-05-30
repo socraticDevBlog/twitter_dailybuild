95d2acae2375142ae334def613ec8be91d0fa096
socraticDevBlog <41236715+socraticDevBlog@users.noreply.github.com>
2022-05-30 19:05:03 -0400

    cheers d3d1rty

--- regular-tweet.js
+++ regular-tweet.js
@@ -6,7 +6,7 @@ const COMMAND_LINE_ARGS_1_IDX = 2;
 // populate the message you want to tweet, then submit a pull-request
 // ** if there is already a message in there: replace it with yours
 //
-var message = "@tacixat another cool game for your garage https://putin.icu/, no??"
+var message = "Cheers to our good fren le @d3d1rty for graduating university in computer stuff🎉! we are proud of you, no??"
 // that's it ! dont worry about the code down under.
 
 // if msg passed as command-line argument, overwrite original message
