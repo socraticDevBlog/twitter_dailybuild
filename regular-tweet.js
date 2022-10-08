9fc6e79dfd63453594c4acae54680a20353b7f84
socraticDevBlog <41236715+socraticDevBlog@users.noreply.github.com>
2022-10-08 12:04:54 -0400

    tween about fox reinecke ocmic strips

--- regular-tweet.js
+++ regular-tweet.js
@@ -6,7 +6,7 @@ const COMMAND_LINE_ARGS_1_IDX = 2;
 // populate the message you want to tweet, then submit a pull-request
 // ** if there is already a message in there: replace it with yours
 //
-var message = "last three movies watched by dailybuilders were Office Space, Dr Strangelove, and Beetlejuice.  Riffing movies with friends is fun https://twitter.dailybuild.org/movies"
+var message = "dailybuilding is also about crafting fun comic strips https://dailybuild.org/images/fox_reineke_rabbits.png #fox #bacon"
 // that's it ! dont worry about the code down under.
 
 // if msg passed as command-line argument, overwrite original message
