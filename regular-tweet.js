d94330823cdcb34256a935b8f27fc78ec5dc8ced
socraticDevBlog <41236715+socraticDevBlog@users.noreply.github.com>
2022-11-18 13:58:08 -0500

    Update regular-tweet.js

--- regular-tweet.js
+++ regular-tweet.js
@@ -6,7 +6,7 @@ const COMMAND_LINE_ARGS_1_IDX = 2;
 // populate the message you want to tweet, then submit a pull-request
 // ** if there is already a message in there: replace it with yours
 //
-var message = "dailybuilding is also about crafting fun comic strips https://dailybuild.org/images/fox_reineke_rabbits.png #fox #bacon"
+var message = "you like tech twitter? join #dailybuild on IRC network Rizon!!  #RIPTwitter #tech"
 // that's it ! dont worry about the code down under.
 
 // if msg passed as command-line argument, overwrite original message
