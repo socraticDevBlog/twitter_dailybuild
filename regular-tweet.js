724c4c39d1b482720ed66b2a9c861f9206b282b6
socraticDevBlog <41236715+socraticDevBlog@users.noreply.github.com>
2022-12-17 13:05:32 -0500

    Update regular-tweet.js

--- regular-tweet.js
+++ regular-tweet.js
@@ -6,7 +6,7 @@ const COMMAND_LINE_ARGS_1_IDX = 2;
 // populate the message you want to tweet, then submit a pull-request
 // ** if there is already a message in there: replace it with yours
 //
-var message = "dailybuild is proud of our member @Soundtoxin who single handedly implemented a new timezone in dailybuild's world clock. be confident in your skills! https://dailybuild.org/#clock"
+var message = "howday @Soundtoxin  havent seen you around le irc. :((  dailybuild hopes everything allright!"
 // that's it ! dont worry about the code down under.
 
 // if msg passed as command-line argument, overwrite original message
