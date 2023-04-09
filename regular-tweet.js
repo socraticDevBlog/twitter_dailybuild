ca975b5551c554ded0e856035bf5517afa831d8e
socraticDevBlog <41236715+socraticDevBlog@users.noreply.github.com>
2023-04-09 10:45:36 -0400

    happyeaster2023tweet

--- regular-tweet.js
+++ regular-tweet.js
@@ -6,7 +6,7 @@ const COMMAND_LINE_ARGS_1_IDX = 2;
 // populate the message you want to tweet, then submit a pull-request
 // ** if there is already a message in there: replace it with yours
 //
-var message = "imagine a comfy IRC channel into the year 2022 and *then* having nice people talking about many things like emacs, linux, and ducks. That is NOT ridiculous"
+var message = "happy easter from your frens :) :) :) https://dailybuild.org/images/happyeaster2023.png"
 // that's it ! dont worry about the code down under.
 
 // if msg passed as command-line argument, overwrite original message
